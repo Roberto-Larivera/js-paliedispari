36834f08cafbbc6441f7524cfe5bb2343fe4ca7d
Roberto Larivera <robylariv@gmail.com>
2023-01-10 18:12:41 +0100

    aggiornato commenti

--- pari_e_dispari/js/scripts.js
+++ pari_e_dispari/js/scripts.js
@@ -18,43 +18,45 @@ function randomComputer (){
 
 function controlNum (min, max, input){                                                  // funzione controllo input numero utente + possibilità di modificare i due numeri min max
     while((input < min) || (input > max) || isNaN(input)){                                 
-        console.log('Numero non valido!');
+        //console.log('Numero non valido!');
+        alert('Numero non valido!');
         input = parseInt(prompt(`Numero non valido! Scegli un numero tra ${min} e ${max}`));
-        console.log('input',input, typeof input);
-        console.log(isNaN(input));
+        //console.log('input',input, typeof input);
+        //console.log(isNaN(input));
     }
 }
 
 function controlVal (input){                                                            // funzione controllo valore di gioco tramite quello scelto dall'utente
     while ((input != 'pari') && (input != 'Pari') && (input != 'dispari') && (input != 'Dispari')){
-        console.log('Valore non valido!');
+        //console.log('Valore non valido!');
+        alert('Valore non valido!');
         input = prompt('Scelta non valida! Scegli tra pari e dispari');
-        console.log('input',input, typeof input);
+        //console.log('input',input, typeof input);
     }
 }
 
 
 let numberUser = parseInt(prompt('Scegli un numero tra 1 e 5'));                          // richiesta numero tra 1 e 5
-console.log('numberUser',numberUser, typeof numberUser);
-console.log(isNaN(numberUser));
+//console.log('numberUser',numberUser, typeof numberUser);
+//console.log(isNaN(numberUser));
 
 controlNum(1,5,numberUser);                                                             // funzione controllo input numero utente + possibilità di modificare i due numeri min max
 
 
 let choiceUser = prompt('Scegli tra pari e dispari');                                    // richiesta se pari o dispari scritto
-console.log('choiceUser',choiceUser, typeof choiceUser);
+//console.log('choiceUser',choiceUser, typeof choiceUser);
 
 controlVal(choiceUser);                                                                 // funzione controllo valore di gioco tramite quello scelto dall'utente
 
 
 const numberComputer = randomComputer();                                                 // numero random per il pc tra 1 e 5
-console.log('numberComputer',numberComputer);
+//console.log('numberComputer',numberComputer);
 
 
 
 
 const numberSum = numberComputer + numberUser;
-console.log('numberSum',numberSum, typeof numberSum);
+//console.log('numberSum',numberSum, typeof numberSum);
 
 if ( numberSum % 2 == 0){
     console.log('Pari');
