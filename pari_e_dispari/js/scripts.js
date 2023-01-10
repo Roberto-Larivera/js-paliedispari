a54e484c45b98acdffe1324f6d01ad07f42affc1
Roberto Larivera <robylariv@gmail.com>
2023-01-10 16:46:45 +0100

    inizializazione esercizio ParieDispari - creato numero random computer

--- pari_e_dispari/js/scripts.js
+++ pari_e_dispari/js/scripts.js
@@ -1,1 +1,9 @@
 console.log('int ok');
+
+function randomComputer (){
+    return Math.floor(Math.random() * (5 - 1 + 1) ) + 1;
+
+}
+
+const numberComputer = randomComputer();
+console.log('numberComputer',numberComputer);
